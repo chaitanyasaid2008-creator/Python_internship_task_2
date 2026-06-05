e829eba3ed31845269efb3205b530517a31330e5
unknown <chaitanyasai2008@gmail.com>
2026-06-05 18:17:09 +0530

    update

--- Employee.py
+++ Employee.py
@@ -9,8 +9,11 @@ def add_employee():
 		emp_salary = int(input("enter employee salary : "))
 		employee ={"emp_id":emp_id,"emp_name":emp_name,"emp_mail":emp_mail,"emp_number":emp_number,"emp_salary":emp_salary}
 		data.append(employee)
-
-
+		print(f"{'emp_id':<10} | {'emp_name':<12} | {'emp_mail':<10} | {'emp_number':<10} | {'emp_salary':<15}")
+		print("-" * 67)
+		for employee in data:
+			    print(f"{employee['emp_id']:<10} | {employee['emp_name']:<12} | {employee['emp_mail']:<10} | {employee['emp_number']:<10} | {employee['emp_salary']:<15}")
+			    
 def upd_employee():
 	m=int(input("no.of employees updation :"))
 	for l in range(m):
@@ -23,19 +26,30 @@ def upd_employee():
 						b=input("enter a change : ")	
 						if b in ['emp_number','emp_salary']:
 							i[b]=int(input(b ))
+							print(f"{'emp_id':<10} | {'emp_name':<12} | {'emp_mail':<10} | {'emp_number':<10} | {'emp_salary':<15}")
+							print("-" * 67)
+							for employee in data:
+								print(f"{employee['emp_id']:<10} | {employee['emp_name']:<12} | {employee['emp_mail']:<10} | {employee['emp_number']:<10} | {employee['emp_salary']:<15}")
+
 							break
 						else:
 							i[b]=input(b )
-							break				
-			
+							print(f"{'emp_id':<10} | {'emp_name':<12} | {'emp_mail':<10} | {'emp_number':<10} | {'emp_salary':<15}")
+							print("-" * 67)
+							for employee in data:
+								print(f"{employee['emp_id']:<10} | {employee['emp_name']:<12} | {employee['emp_mail']:<10} | {employee['emp_number']:<10} | {employee['emp_salary']:<15}")
 
-
+							break
+				
 def search_employee():
 	emp_id2=input("enter employee id : ")
 	for i in data:
 		if i["emp_id"]==emp_id2 :
 			print("employee details : ",i)
-
+			print(f"{'emp_id':<10} | {'emp_name':<12} | {'emp_mail':<10} | {'emp_number':<10} | {'emp_salary':<15}")
+			print("-" * 67)
+			for employee in data:
+				print(f"{employee['emp_id']:<10} | {employee['emp_name']:<12} | {employee['emp_mail']:<10} | {employee['emp_number']:<10} | {employee['emp_salary']:<15}")
 
 def salary_employee():
 	m=int(input("no.of employees :"))
@@ -47,23 +61,28 @@ def salary_employee():
 				print("employee salary :",i["emp_salary"])
 				print("total salary :",i["emp_salary"]+bonus)
 				i["total_salary"]=i["emp_salary"]+bonus
-m=int(input("enter no.of choices :"))
-for i in range(m):
+				print(f"{'emp_id':<10} | {'emp_name':<12} | {'emp_mail':<10} | {'emp_number':<10} | {'emp_salary':<15} | {'total_salary':<15}")
+				print("-" * 80)
+				for employee in data:
+					print(f"{employee['emp_id']:<10} | {employee['emp_name']:<12} | {employee['emp_mail']:<10} | {employee['emp_number']:<10} | {employee['emp_salary']:<15} | {employee['total_salary']:<15}")
+
+for i in range(4):
 	choice=int(input("\n 1.add_employees \n 2.update_employees \n 3.search_employees \n 4.salary_employees \n"))
-	if choice==1 :
-		add_employee()
-		print(data)
-	elif choice==2 :
-		upd_employee()
-		print(data)
-	elif choice==3 :
-		search_employee()
-		print(data)
-	elif choice==4 :
-		salary_employee()
-		print(data)
-	else :
-		print("invalid choice")
+	while choice:
+		if choice==1 :
+			add_employee()
+			break
+		elif choice==2 :
+			upd_employee()
+			break
+		elif choice==3 :
+			search_employee()
+			break
+		elif choice==4 :
+			salary_employee()
+			break
+		else :
+			print("invalid choice")
 
 
 		
